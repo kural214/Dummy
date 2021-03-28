f069c9d65fe2eb72084e1c04e2130f9ce2b66d47
Kural <kuralarasan.muthusamy@syncfusion.com>
2021-03-28 09:53:40 +0530

    Random Commit

--- upload.bat
+++ upload.bat
@@ -1,9 +1,12 @@
 @ECHO OFF
 git init
-git remote add origin https://kuralmkural@gmail.com:Syncfusion@123@github.com/kural214/Dummy.git
+git remote add origin https://github.com/kural214/Dummy.git
 git add .
 git commit -m "Random Commit"
+git config credential.helper store
 git push origin master
+Username: kuralmkural@gmail.com
+Password: Syncfusion@123
 
 
 PAUSE
