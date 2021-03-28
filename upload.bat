d523c1bb572b84557a1777bc4da91227bc23bd83
Kural <kuralarasan.muthusamy@syncfusion.com>
2021-03-28 10:05:13 +0530

    Random Commit

--- upload.bat
+++ upload.bat
@@ -1,10 +1,9 @@
 @ECHO OFF
 git init
-git config --global credential.helper wincred
+git config --global credential.helper store
 git remote add origin https://github.com/kural214/Dummy.git
 git add .
 git commit -m "Random Commit"
-git config credential.helper store
 git push origin master
 
 
